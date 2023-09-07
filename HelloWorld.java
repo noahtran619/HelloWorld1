f56d9bb46d9b038bd0906f44e514116247565125
Noah Tran <noah.tran@mrjaffesclass.com>
2023-09-07 10:53:37 -0700

    Added Data Types

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld
     public static void main(String[] args) 
     {
         System.out.println("hello world") ;
+        int i;
+        double f = 2.5;
+        boolean flag = true;
     }
 }
 
