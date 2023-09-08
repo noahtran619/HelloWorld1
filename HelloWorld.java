da9923417b15ded5ddb65ca4dbcbdeca170a51e6
Noah Tran <noah.tran@mrjaffesclass.com>
2023-09-08 11:07:32 -0700

    Added Operators assignment

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,10 +3,17 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
-        System.out.println("hello world") ;
+        System.out.println("hello gang") ;
         int i;
         double f = 2.5;
         boolean flag = true;
+        i = 10;
+        f += i;
+        flag = !flag;
+        i++;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
     }
 }
 
