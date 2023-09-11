c5f82edba69675250371c373203229bcbf2122c1
Noah Tran <noah.tran@mrjaffesclass.com>
2023-09-11 11:43:38 -0700

    Added Strings

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,6 +13,14 @@ public class HelloWorld
         System.out.println(++i);
         System.out.println(f);
         System.out.println(flag);
+        String string1 = "I am a string";
+        String string2 = string1;
+        String string3 = "I am also a string";
+        boolean stringsEqual = string1.equals(string3);
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
+        System.out.println(stringsEqual);
     }
 }
 
